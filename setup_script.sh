ce368fb095e09e1ff094b8693052b05ddc019242
Ubuntu <ubuntu@ip-172-31-22-255.us-west-2.compute.internal>
2013-07-18 21:58:02 +0000

    updated setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -2,15 +2,15 @@
 name="Orion Osborn"
 email="orion.osborn@gmail.com"
 
-sudo apt-get install -y git-core
 git config --global user.name $name
 git config --global user.email $email
 git clone https://github.com/startup-class/setup.git
 ./setup/setup.sh  
-git clone https://github.com/oorion/bitstarter/
-cd bitstarter
 sudo apt-get update
 sudo apt-get install -y python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install -y nodejs
+
+git clone https://github.com/oorion/bitstarter/
+cd bitstarter
